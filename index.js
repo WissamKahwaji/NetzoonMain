a26f8aeb9ed13b0589a58bff9ddb5942db46f2a6
WissamKahwaji <kahwajiwisam@gmail.com>
2024-09-22 14:42:45 +0400

    edits sizes json

--- index.js
+++ index.js
@@ -113,6 +113,8 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "2gb" }));
+app.use(express.urlencoded({ limit: "2gb", extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/images", express.static(path.join(__dirname, "images")));
